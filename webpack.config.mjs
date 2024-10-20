cb29fb297c067ee1015c9afd7b61ac82639a840c
iZeevens <izeevens@gmail.com>
2024-10-20 12:33:41 +0500

    feat: add prod & dev mode for project

---
+++ webpack.config.mjs
@@ -0,0 +1,27 @@
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+  entry: './src/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts'],
+  },
+  mode: process.env.NODE_ENV || 'development',
+  target: 'node',
+};
